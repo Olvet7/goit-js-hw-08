b2e0d7a0f2081cc7772a855bf4175c5cbb834f0d
Olvet7 <volya.maslova@gmail.com>
2023-11-09 14:22:03 +0200

    try

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+// Задаємо час затримки для функції throttle
+const THROTTLE_DELAY = 1000;
+
+// змінна для зберігання часу відтворення
+const storageKey = 'videoplayer-current-time';
+
+// Прослуховуємо подію в плеєрі, прикріпивши зворотний виклик за допомогою .on() + throttle із функцією оновлення в Local Storage 
+player.on('timeupdate', throttle(onPlayerTimeChange, THROTTLE_DELAY));
+
+// Визначаємо функцію onPlayerTimeChange, яка викликається при кожному оновленні часу відтворення
+function onPlayerTimeChange(event) {
+  const time = Math.floor(event.seconds); // заокруглюємо кількість секунд
+  localStorage.setItem(storageKey, time); // записуємо у Local Storage
+}
+
+const pausedTime = localStorage.getItem(storageKey);
+
+if (pausedTime) {
+  player.setCurrentTime(pausedTime || 0);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+// обмеження кількості викликів функції у заданому інтервалі часу.
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
+
+// об'єкт data містить email та message і зберігає цей об'єкт в локальному сховищі під ключем STORAGE_KEY.
+const saveData = () => {
+  const data = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+};
+
+// зчитує об'єкт data і заповнює поля email та message значеннями, якщо поля були заповнені
+const updateFormData = () => {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  emailInput.value = data.email || '';
+  messageInput.value = data.message || '';
+};
+
+// перевіряє на зміст форми, видаляє дані з локального сховища і очищає поля форми після відправки
+const submitForm = event => {
+  event.preventDefault();
+  const data = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  if (emailInput.value === '' || messageInput.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(data);
+  localStorage.removeItem(STORAGE_KEY);
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+emailInput.addEventListener('input', throttle(saveData, 500));
+messageInput.addEventListener('input', throttle(saveData, 500));
+form.addEventListener('submit', submitForm);
+
+// відновлення форми, якщо вона була заповнена
+updateFormData();
